487a99384adf67aac99470d969ef77c85c886f28
RED9839 <wnsdud768@gmail.com>
2026-05-20 15:56:43 +0900

    시뮬레이터 페이지 lazy loader 적용

--- app/simulator/page.tsx
+++ app/simulator/page.tsx
@@ -1,16 +1,14 @@
 import { Suspense } from "react";
-import SimulatorPageClient from "./SimulatorPageClient";
+import SimulatorClientLoader from "./SimulatorClientLoader";
 
 function SimulatorPageFallback() {
-  return (
-    <main className="min-h-screen bg-[#03060b]" />
-  );
+  return <main className="min-h-screen bg-[#03060b]" />;
 }
 
 export default function SimulatorPage() {
   return (
     <Suspense fallback={<SimulatorPageFallback />}>
-      <SimulatorPageClient />
+      <SimulatorClientLoader />
     </Suspense>
   );
 }
